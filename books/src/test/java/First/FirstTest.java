a48ddf0eeb105fe3306e8a77d5da0c4505b711a1
Oliver Wack <oliver.wack@trivadis.com>
2020-03-06 08:55:56 +0100

    Added junit an wrote simple test

---
+++ books/src/test/java/First/FirstTest.java
@@ -0,0 +1,16 @@
+package First;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+	
+	@Test
+	public void addTest() {
+		int one = 1;
+		int test = 1;
+		assertTrue(one == test);
+	}
+
+}
